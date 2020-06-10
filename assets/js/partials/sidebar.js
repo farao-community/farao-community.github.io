0d047ceba7d021ce8f198120cbed83c6ac199347
EDWARDS Philippe <philippe.edwards@rte-france.com>
2020-06-10 10:43:49 +0200

    working but not very clean

--- assets/js/partials/sidebar.js
+++ assets/js/partials/sidebar.js
@@ -1,8 +1,16 @@
 function changeContentTop() {
     const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-    if(vw < 1200){
-        var sidebar = document.getElementById('sidebar');
+
+    var sidebar = document.getElementById('sidebar');
+
+    var image = document.getElementById('main');
+    var header = document.querySelector(".site-header");
+    sidebar.style.marginTop = (header.offsetHeight + image.offsetHeight + 20) + "px";
+
+    if(vw < 1200) {
         var sidebarHeight = sidebar.offsetHeight;
-        document.querySelector(".post-content").style.marginTop = sidebarHeight + "px";
+        document.querySelector(".post-content").style.marginTop = (sidebarHeight + 10) + "px";
+    } else {
+        document.querySelector(".post-content").style.marginTop = "0px";
     }
 }
